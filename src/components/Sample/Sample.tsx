9eb919f008119eb7dfc69d1e946d7a7d1cd66598
FAL-coffee <fal.engineer.2001@gmail.com>
2022-09-20 20:50:47 +0900

    :hammer: jest導入

---
+++ src/components/Sample/Sample.tsx
@@ -0,0 +1,5 @@
+export const Sample = () => {
+  return (
+    <p>sample</p>
+  );
+};
---
+++ src/components/Sample/test.tsx
@@ -0,0 +1,9 @@
+import { Sample } from "~/components/Sample";
+import { render } from "@testing-library/react";
+
+describe("<Sample />", () => {
+  test("render test", () => {
+    const { getAllByText } = render(<Sample />);
+    expect(getAllByText('sample')).toBeTruthy()
+  });
+});
