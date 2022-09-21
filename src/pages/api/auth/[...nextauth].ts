b605999ae3bcc2536fcf62d6f5be33abcea00464
FAL-coffee <fal.engineer.2001@gmail.com>
2022-09-21 23:35:10 +0900

    :fire: remove unless provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,9 @@
 import NextAuth from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 import { env } from 'src/env/server.mjs'
 
 export default NextAuth({
   providers: [
-    GithubProvider({
-      clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET,
-    }),
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
