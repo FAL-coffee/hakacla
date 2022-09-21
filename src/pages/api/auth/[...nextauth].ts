1045625997d27b698eee15afaf0da0b598f15164
FAL-coffee <fal.engineer.2001@gmail.com>
2022-09-22 03:49:35 +0900

    :hammer: client settings

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,14 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { env } from 'src/env/server.mjs'
 
-export default NextAuth({
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-})
+}
+
+export default NextAuth(authOptions)
